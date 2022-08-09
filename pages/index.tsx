4b64827af7eb74c1cdd1e79a837d26a400b61943
riyuzenn <riyuzenn@gmail.com>
2022-08-09 10:04:42 +0200

    upt: add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from "next";
 import Banner from "../components/banner";
+import PageHead from "../components/head";
 
 const IndexPage: NextPage = () => {
     return (
         <>
+            <PageHead title="riyuzenn – full stack dev" />
             <Banner />
         </>
     );
