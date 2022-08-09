93a4300e53a48ea4e4267431a0b594ee9c05840a
riyuzenn <riyuzenn@gmail.com>
2022-08-09 09:52:50 +0200

    feat: add commit script

---
+++ commit.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+run_format() {
+    npm run format
+}
+commit() {
+    git commit -m "$commit"
+    git push origin mater
+}
+main() {
+    run_format
+    git add .
+    echo
+    read -r -p "Enter commit message: " commit
+    echo "Commit message: $commit"
+    while true
+    do
+        read -r -p "Are you sure you want to commit & push? [Y/n] " input
+    
+        case $input in
+                [yY][eE][sS]|[yY])
+                    # echo "Yes"
+                    commit
+                    break
+                    ;;
+                [nN][oO]|[nN])
+                    # echo "No"
+                    break
+                    ;;
+                *)
+                    # echo "Invalid input..."
+                    ;;
+        esac      
+    done
+}
+
+main
