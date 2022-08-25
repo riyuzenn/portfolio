d838da0397a3550eb4964a06c65d36e6232104b2
riyuzenn <riyuzenn@gmail.com>
2022-08-25 22:31:38 +0800

    feat: add lorem page

---
+++ pages/lorem.tsx
@@ -0,0 +1,25 @@
+import { NextPage } from "next";
+import Typed from "react-typed";
+
+const LoremPage : NextPage = () => {
+    return (
+        <div className="flex h-screen justify-center items-center">
+            <Typed
+                className="text-xl"
+                strings={[
+                    'hello there 👋', 
+                    'it looks like you\'ve enter a void of space.', 
+                    'quick, get out of here as soon as you can...',
+                    '...',
+                    'bye 👋',
+                    '...'
+                ]}
+                typeSpeed={30}
+                backSpeed={20}
+            />
+            
+        </div>
+    )
+}
+
+export default LoremPage;
