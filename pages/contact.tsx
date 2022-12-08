54ea0c301d9801f21f1afa9f78582bd05c11a520
riyu <66673988+riyuzenn@users.noreply.github.com>
2022-12-08 22:10:23 +0800

    Update contact.tsx

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -104,7 +104,7 @@ const ContactPage: NextPage = () => {
                                     href="https://discord.com/users/952728445010460682"
                                     className="text-[#ac8aac] hover:underline"
                                 >
-                                    riyu#1529
+                                    riyu#8996
                                 </a>
                             </div>
                         </div>
